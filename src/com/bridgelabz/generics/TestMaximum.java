af02c05f7286e946931b4a29fe679a8967431843
mk3709 <mk3709@srmist.edu.in>
2024-02-02 12:43:57 +0530

    RefactorDone

--- src/com/bridgelabz/generics/TestMaximum.java
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -1,61 +1,46 @@
 package com.bridgelabz.generics;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Optional;
 
-public class TestMaximum {
 
-    public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
+class TestMaximum<T extends Comparable<? super T>>{
+     T  value1;
+     T  value2;
+     T value3;
 
+     public TestMaximum(T value1, T value2, T value3)
 
-            T max = x;
-
-            if (y.compareTo(max) > 0) {
-                max = y;
+    {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+            public T findMax()
+            {
+                return TestMaximum.findMax(value1,value2,value3);
             }
 
-            if (z.compareTo(max) > 0) {
-                max = z;
+            public static <T extends Comparable<? super T>> T findMax(T num1,T num2,T num3){
+
+               return Optional.of(num1).stream().max((a, b)->a.compareTo(b)).orElse(null);
+
             }
-            return max;
-        }
 
 
         public static void main (String[]args){
             // Test Case 1: Max at 1st position
+         TestMaximum<Integer> integerTestMaximum= new TestMaximum<>(10,5,8);
+         Integer maxResult = integerTestMaximum.findMax();
+            System.out.println("Max Result"+maxResult);
 
-            Integer max1 = findMax(5, 3, 2);
-            System.out.println("Test Case 1 - Maximum: " + max1);
+            TestMaximum<Float> floatTestMaximum= new TestMaximum<>(0.7f,0.6f,0.8f);
+            Float maxFloatResult = floatTestMaximum.findMax();
+            System.out.println("Max Result"+maxFloatResult);
 
-
-            Integer max2 = findMax(3, 5, 2);
-            System.out.println("Test Case 1 - Maximum: " + max2);
-
-            Integer max3 = findMax(4, 8, 6);
-            System.out.println("Test Case 2 - Maximum: " + max3);
-
-
-            Float max4 = findMax(0.8f, 0.4f, 0.6f);
-            System.out.println("Test Case 1 - Maximum: " + max4);
-
-
-            // Test Case 2: Max at 2nd position
-
-            Float max5 = findMax(0.4f, 0.8f, 0.6f);
-            System.out.println("Test Case 2 - Maximum: " + max5);
-
-            // Test Case 3: Max at 3rd position
-            Float max6 = findMax(0.1f, 0.3f, 0.7f);
-            System.out.println("Test Case 3 - Maximum: " + max6);
-
-
-            String max7 = findMax("Apple", "Peach", "Banana");
-            System.out.println("Test Case 1 - Maximum: " + max7);
-
-
-            String max8 = findMax("Apple", "Peach", "Banana");
-            System.out.println("Test Case 2 - Maximum: " + max8);
-
-            String max9 = findMax("Banana", "Apple", "Peach");
-            System.out.println("Test Case 3 - Maximum: " + max9);
-
-
+         TestMaximum<String> stringTestMaximum= new TestMaximum<>("Apple","Peach","Banana");
+         String maxStringResult = stringTestMaximum.findMax();
+            System.out.println("Max Result"+maxStringResult);
         }
     }
