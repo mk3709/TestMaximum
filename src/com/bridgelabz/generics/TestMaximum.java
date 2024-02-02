00197a8c324c717e370687cbe87f5cf970ec7312
mk3709 <mk3709@srmist.edu.in>
2024-02-02 10:10:16 +0530

    UC1-added

--- src/com/bridgelabz/generics/TestMaximum.java
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -1,28 +1,32 @@
 package com.bridgelabz.generics;
 
 public class TestMaximum {
-    public static void main(String[] args) {
+            // Generic method to find the maximum of three objects
+            public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
+                T max = x;
 
+                if (y.compareTo(max) > 0) {
+                    max = y;
+                }
 
-        System.out.println("Test case 1: " + testMaxNumber(7, 3, 5));
-        System.out.println("Test case 2: " + testMaxNumber(2,8,5));
-        System.out.println("Test case 3: " + testMaxNumber(6, 9, 12));
+                if (z.compareTo(max) > 0) {
+                    max = z;
+                }
 
-    }
-        // Method to find the maximum of three integers
-        public static int testMaxNumber(Integer x, Integer y, Integer z) {
-            Integer max = x;
-
-            if (y.compareTo(max) > 0) {
-                max = y;
+                return max;
             }
 
-            if (z.compareTo(max) > 0) {
-                max = z;
-            }
+            public static void main(String[] args) {
+                // Test Case 1: Max at 1st position
+                Integer max1 = findMax(5, 3, 2);
+                System.out.println("Test Case 1 - Maximum: " + max1);
 
-            return max;
+                // Test Case 2: Max at 2nd position
+                Integer max2 = findMax(4, 8, 6);
+                System.out.println("Test Case 2 - Maximum: " + max2);
+
+                // Test Case 3: Max at 3rd position
+                Integer max3 = findMax(1, 3, 7);
+                System.out.println("Test Case 3 - Maximum: " + max3);
+            }
         }
-    }
-
-
