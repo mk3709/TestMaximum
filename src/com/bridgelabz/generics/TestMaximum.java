da5e693cac1927bcb9f231e3ca5f2486f19f631a
mk3709 <mk3709@srmist.edu.in>
2024-02-02 10:20:58 +0530

    UC2-MaximumFloat

--- src/com/bridgelabz/generics/TestMaximum.java
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -1,17 +1,11 @@
 package com.bridgelabz.generics;
 
 public class TestMaximum {
-    public static void main(String[] args) {
 
+        // Generic method to find the maximum of three objects
+        public static Float  findMax(Float x, Float y, Float z) {
 
-        System.out.println("Test case 1: " + testMaxNumber(7, 3, 5));
-        System.out.println("Test case 2: " + testMaxNumber(2,8,5));
-        System.out.println("Test case 3: " + testMaxNumber(6, 9, 12));
-
-    }
-        // Method to find the maximum of three integers
-        public static int testMaxNumber(Integer x, Integer y, Integer z) {
-            Integer max = x;
+            Float max = x;
 
             if (y.compareTo(max) > 0) {
                 max = y;
@@ -23,6 +17,18 @@ public class TestMaximum {
 
             return max;
         }
+
+        public static void main(String[] args) {
+            // Test Case 1: Max at 1st position
+            Float max1 = findMax(0.5f, 0.3f, 0.2f);
+            System.out.println("Test Case 1 - Maximum: " + max1);
+
+            // Test Case 2: Max at 2nd position
+            Float max2 = findMax(0.4f, 0.8f, 0.6f);
+            System.out.println("Test Case 2 - Maximum: " + max2);
+
+            // Test Case 3: Max at 3rd position
+            Float max3 = findMax(0.1f, 0.3f, 0.7f);
+            System.out.println("Test Case 3 - Maximum: " + max3);
+        }
     }
-
-
